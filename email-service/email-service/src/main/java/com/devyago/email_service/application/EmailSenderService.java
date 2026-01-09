d69e6b4423e725c2418c2749afae5ae8289e4eb3
YagoSantoss <yagomib971@gmail.com>
2026-01-09 12:47:30 -0300

    add files

---
+++ email-service/email-service/src/main/java/com/devyago/email_service/application/EmailSenderService.java
@@ -0,0 +1,22 @@
+package com.devyago.email_service.application;
+
+import com.devyago.email_service.adapters.EmailSenderGateway;
+import com.devyago.email_service.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.SendEmail(to, subject, body);
+
+    }
+}
---
+++ email-service/email-service/src/main/java/com/devyago/email_service/core/EmailSenderUseCase.java
@@ -0,0 +1,7 @@
+package com.devyago.email_service.core;
+
+public interface EmailSenderUseCase {
+
+    void sendEmail(String to,String subject, String body);
+
+}
